31ae2e036292bce57b65e5bb2b5727b0bf3ebd4c
Bartosz Jakubowski <bartoszj321@gmail.com>
2022-10-30 23:18:36 +0100

    Google sheet save commit

--- Wózek_skrypt.js
+++ Wózek_skrypt.js
@@ -1,156 +1,82 @@
-const  email = Session.getActiveUser().getEmail();
-const sheetGrafik = SpreadsheetApp.getActiveSpreadsheet().getSheetByName("Roboczy 2");
+//const  personData = Session.getActiveUser();
+//const sheetGrafik = SpreadsheetApp.getActiveSpreadsheet().getSheetByName("ROBOCZY");
 const sheetOsoby = SpreadsheetApp.getActiveSpreadsheet().getSheetByName("Administrator");  
 
-var person = getuserData(sheetOsoby);
-   
+var user = {
+        userName: undefined,
+        userMail: undefined,
+        isAdmin : undefined
+      }
 
-function onOpen(){
- isAdmin(sheetOsoby);
-  //dropList(sheetOsoby);
+function onOpen()
+{
+  if (SpreadsheetApp.getActiveSpreadsheet().getSheetName() == "ROBOCZY")
+  {
+      getUserData(sheetOsoby);     
+
+      console.log(user.userName);
+      console.log(user.userMail);
+      console.log(user.isAdmin);
+  }
 }
 
-function onEdit(e){
 
-const rangeModifed = e.range;
-SpreadsheetApp.getActive().toast(SpreadsheetApp.getUi().alert(`${rangeModifed.getValue} `, SpreadsheetApp.getUi().ButtonSet.OK));
 
-//  editedCell(person.name, sheetGrafik); 
-}
-function onSelectionChange(e){
+//przed edycją zapisz cały sheet -> po edycji i validacji będzie kopia zapasowa 
+
+
+
+
+
+
+
+
+
+
+
+
+
+function onSelectionChange(){
+  var cell = SpreadsheetApp.getActiveSheet().getActiveCell().getValue();
+  console.log(cell);
   SpreadsheetApp.getActive().toast(SpreadsheetApp.getUi().alert("działa", SpreadsheetApp.getUi().ButtonSet.OK));
 }
 
 
-    
+function onEdit(e){
+
+if (SpreadsheetApp.getActiveSpreadsheet().getSheetName() == "ROBOCZY")
+{
+  const rangeModifed = e.range;
+  console.log(rangeModifed)
+  SpreadsheetApp.getActive().toast(SpreadsheetApp.getUi().alert(rangeModifed.getValues(), SpreadsheetApp.getUi().ButtonSet.OK));
+
+  if (rangeModifed.getValues().toString() == user.userName)
+  {
+    //SpreadsheetApp.getActive().toast(SpreadsheetApp.getUi().alert(`Masz uprawnienia do edycji: ${rangeModifed.getValues()}`, SpreadsheetApp.getUi().ButtonSet.OK));
+  }
+  else
+  {
+    //SpreadsheetApp.getActive().toast(SpreadsheetApp.getUi().alert(`Nie masz uprawnień: ${rangeModifed.getValues()}`, SpreadsheetApp.getUi().ButtonSet.OK));
+  }
+
+}
+}
 
 
+function getUserData(sheet) {
 
-function isAdmin(sheet) {
 const data = sheet.getDataRange().getValues();
+const userName = Session.getUser().getUserLoginId();
 
   for (i = 0; i < data.length; i++)
   {
-    if(data[i][1] == email)
+    if(data[i][1] == userName)
     {
-        if(data[i][2] == true)
-          return true;
-
-        else if (data[i][2] == 'special'){
-          var alert = SpreadsheetApp.getUi().alert("Na kolana i ładnie proś", SpreadsheetApp.getUi().ButtonSet.OK);
-          SpreadsheetApp.getActive().toast(alert);
-          return false;
-        }
-
-        else
-          return false;
+       user.userName = data[i][0];
+       user.userMail = data[i][1];
+       user.isAdmin = data[i][2];
     }  
   }
-  //if email is not aprovved
-  var alert = SpreadsheetApp.getUi().alert("Ups 😬\nWygląda, że Ciebie nie zapisaliśmy 😲\nSkontaktuj się z Ryszardem Jakubowskim 😁", SpreadsheetApp.getUi().ButtonSet.OK);
-
-  SpreadsheetApp.getActive().toast(alert);
 
 }
-
-
-function getuserData(sheet){
-const data = sheet.getDataRange().getValues();
-
-  for (i = 0; i < data.length; i++)
-  {
-    if(data[i][1] == email)
-        return person = {name: data[i][0], mail: data[i][1], isAdmin: data[i][2]};
-  }
-}
-
-
-function editedCell(person, sheet){
-  var cell = sheet.getDataRange().getActive().getValue();
-  
-SpreadsheetApp.getActive().toast(SpreadsheetApp.getUi().alert(`Średnio działa ${cell.toString()}?`, SpreadsheetApp.getUi().ButtonSet.OK));
-return;
-
-
-    if (cell.getValue() != person.name){
-      return SpreadsheetApp.getActive().toast(SpreadsheetApp.getUi().alert(`Czy na pewno chcesz nadpisać siebie zamiastr ${(cell.getValue).toString()}?`, SpreadsheetApp.getUi().ButtonSet.OK));
-    }
-    else if (cell.getValue() == person.name){
-       
-      return SpreadsheetApp.getActive().toast(SpreadsheetApp.getUi().alert("Zapisałeś się na wózek", SpreadsheetApp.getUi().ButtonSet.OK));
-    }
-    else{
-      return;
-    }
-
-}
-
-
-
-
-
-
-
-
-
-
-// to do?
-
-
-//  function dropList(sheetOsoby){
-   
-   
-//    var cell = SpreadsheetApp.getActiveSpreadsheet().getSelection().getCurrentCell();
-   
-//    if (cell.canEdit() == false ^ cell.isPartOfMerge() == true)
-//    return;
-   
-
-//    console.log(cell);
-//  // sheetOsoby.getRange("D2").setValue("coś tam");
-//  //  sheetOsoby.getRange("D2").setNumberFormat('@STRING@').setValue("coś tam");
-
-
-
-//  }
-
-
-//  function createTimeDrivenTriggers() {
-//   // Trigger every 6 hours.
-//   ScriptApp.newTrigger('myFunction')
-//       .timeBased()
-//       .everyHours(6)
-//       .create();
-//   // Trigger every Monday at 09:00.
-//   ScriptApp.newTrigger('myFunction')
-//       .timeBased()
-//       .onWeekDay(ScriptApp.WeekDay.MONDAY)
-//       .atHour(9)
-//       .create();
-// }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
